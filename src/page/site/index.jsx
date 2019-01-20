1d8f105d691db574022d05b0382e7697422b3ff8
wxh_leo <754557634@qq.com>
2019-01-20 15:26:44 +0800

    添加表单

--- src/page/site/index.jsx
+++ src/page/site/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Menu, Icon, Layout} from 'antd'
+import {Menu, Icon, Layout, Input} from 'antd'
 import 'antd/dist/antd.css'
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -15,70 +15,44 @@ export default class Index extends Component {
   render () {
     return (
       <Layout>
-    <Sider style={{
-      overflow: 'auto', height: '100vh', position: 'fixed', left: 0,
-    }}
-    >
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1">
-          <Icon type="user" />
-          <span className="nav-text">nav 1</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="video-camera" />
-          <span className="nav-text">nav 2</span>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="upload" />
-          <span className="nav-text">nav 3</span>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="bar-chart" />
-          <span className="nav-text">nav 4</span>
-        </Menu.Item>
-        <Menu.Item key="5">
-          <Icon type="cloud-o" />
-          <span className="nav-text">nav 5</span>
-        </Menu.Item>
-        <Menu.Item key="6">
-          <Icon type="appstore-o" />
-          <span className="nav-text">nav 6</span>
-        </Menu.Item>
-        <Menu.Item key="7">
-          <Icon type="team" />
-          <span className="nav-text">nav 7</span>
-        </Menu.Item>
-        <Menu.Item key="8">
-          <Icon type="shop" />
-          <span className="nav-text">nav 8</span>
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout style={{ marginLeft: 200 }}>
-      <Header style={{ background: '#fff', padding: 0 ,textAlign: 'center',fontSize:'30px'}}>送餐系统</Header>
-      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-        <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
-          ...
-          <br />
-          Really
-          <br />...<br />...<br />...<br />
-          long
-          <br />...<br />...<br />...<br />...<br />...<br />...
-          <br />...<br />...<br />...<br />...<br />...<br />...
-          <br />...<br />...<br />...<br />...<br />...<br />...
-          <br />...<br />...<br />...<br />...<br />...<br />...
-          <br />...<br />...<br />...<br />...<br />...<br />...
-          <br />...<br />...<br />...<br />...<br />...<br />...
-          <br />...<br />...<br />...<br />...<br />...<br />
-          content
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Ant Design ©2018 Created by Ant UED
-      </Footer>
-    </Layout>
-  </Layout>
+        <Sider style={{overflow: 'auto', height: '100vh', position: 'fixed', left: 0,}}>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1">
+              <Icon type="home" />
+              <span className="nav-text">首页</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="user" />
+              <span className="nav-text">商家信息</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="profile" />
+              <span className="nav-text">订单信息</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout style={{ marginLeft: 200 }}>
+        <Header style={{ background: '#fff', padding: 0 ,textAlign: 'center',fontSize:'30px'}}>送餐系统</Header>
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+          <Input.Group className="store">
+            <div className="store-info">
+              <label>店铺名称</label>
+              <Input placeholder="请输入商家名称" style={{width: '240px'}}/>
+            </div>
+            <div className="store-info">
+              <label>联系电话</label>
+              <Input placeholder="请输入联系电话" style={{width: '240px'}}/>
+            </div>
+            <div className="store-info">
+              <label>商家地址</label>
+              <Input placeholder="请输入商家地址" style={{width: '240px'}}/>
+            </div>
+          </Input.Group>
+          </div>
+        </Content>
+      </Layout>
+      </Layout>
     )
   }
 }
--- src/router/App.js
+++ src/router/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import '@/style/style.scss'
 import SiteIndex from '@/page/site/index'
 
 export default class App extends Component {
