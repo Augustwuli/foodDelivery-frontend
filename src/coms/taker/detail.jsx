6390c6360f1cd59ad6b462e0334f907fcfbb7172
wxh_leo <754557634@qq.com>
2019-01-27 15:34:22 +0800

    获取订单详情

--- src/coms/taker/detail.jsx
+++ src/coms/taker/detail.jsx
@@ -8,8 +8,12 @@ export default class Detail extends Component {
     super(props)
     this.state = {
       orderId: props.location.state.id,
+      name: '',
       phone: '',
       address: '',
+      storeName: '',
+      storePhone: '',
+      storeAddress: ''
     }
   }
 
@@ -22,11 +26,15 @@ export default class Detail extends Component {
     Api.get(`orders/takerinfo/${orderId}`, null, r => {
       console.log(r)
       this.setState({
+        name: r.data.name,
         phone: r.data.phone,
         address: r.data.address,
+        storeName: r.data.storeName,
+        storePhone: r.data.storePhone,
+        storeAddress: r.data.storeAddress,
         statu: r.statu,
       },function(){
-        console.log('getdata'+this.state.address,this.state.phone)
+        console.log('getdata'+this.state)
       })
     })
   }
@@ -36,12 +44,28 @@ export default class Detail extends Component {
       <BasicLayout>
        <Input.Group className="store">
           <div className="store-info">
-            <label>联系电话</label>
+            <label>收货人</label>
+            <span>{this.state.name}</span>
+          </div>
+          <div className="store-info">
+            <label>收货人联系电话</label>
             <span>{this.state.phone}</span>
           </div>
           <div className="store-info">
-            <label>派送地址</label>
+            <label>收货地址</label>
             <span>{this.state.address}</span>
+          </div>
+          <div className="store-info">
+            <label>商家</label>
+            <span>{this.state.storeName}</span>
+          </div>
+          <div className="store-info">
+            <label>商家联系电话</label>
+            <span>{this.state.storePhone}</span>
+          </div>
+          <div className="store-info">
+            <label>商家地址</label>
+            <span>{this.state.storeAddress}</span>
           </div>
           <div id="container" style={{width: '400px', height: '300px'}}></div> 
           <div id="searchResultPanel" style={{border: '1px solid #C0C0C0', width: '150px', height: 'auto', display: 'none'}}></div>
